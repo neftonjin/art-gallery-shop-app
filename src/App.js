65084366fc1de6ceb905b6e0525a5c5b4b16dc1d
JXG052 <jxg0521@gmail.com>
2023-03-22 19:32:53 +0000

    Added Album template and Signin template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import { Breadcrumbs } from '@mui/material';
+import Album from './testComponents/Album'
+import SignIn from './testComponents/SignIn'
 
-function App() {
+export default function MyApp() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SignIn />
     </div>
   );
 }
-
-export default App;
